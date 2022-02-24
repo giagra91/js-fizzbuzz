a405f34c184070751e02505f1db76f498e3753c9
giagra91 <giagra91@gmail.com>
2022-02-24 15:44:55 +0100

    alternativa struttura JS

--- js/main.js
+++ js/main.js
@@ -2,39 +2,61 @@
 let wrapper = document.getElementById("fizzbuzz-wrapper");
 wrapper.classList.add("d-flex", "bg-success", "flex-wrap", "justify-content-center" );
 
+// document-getElementById("create-cycle").addEventListener("click", function(){
+    
+// })
 
 for (let i = 1; i <= 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0)
     {
-        let newDiv = document.createElement(`div`);
-        newDiv.classList.add(`box-blue`);
-        newDiv.innerHTML = "Fizzbuzz";
-        wrapper.append(newDiv);
+        wrapper.innerHTML += `<div class="box-blue">Fizzbuzz</div>`;
         console.log("FizzBuzz");
 
     } else if (i % 5 == 0) 
     {
-        let newDiv = document.createElement(`div`);
-        newDiv.classList.add(`box-red`);
-        newDiv.innerHTML = "Buzz";
-        wrapper.append(newDiv);
+        wrapper.innerHTML += `<div class="box-red">Buzz</div>`;
         console.log("Buzz");
     } else if (i % 3 == 0)
     {
-        let newDiv = document.createElement(`div`);
-        newDiv.classList.add(`box-blue`);
-        newDiv.innerHTML = "Fizz";
-        wrapper.append(newDiv);
+        wrapper.innerHTML += `<div class="box-blue">Fizz</div>`;
         console.log("Fizz");
 
     } else 
     {
-        let newDiv = document.createElement(`div`);
-        newDiv.classList.add(`box-red`);
-        newDiv.innerHTML = i;
-        wrapper.append(newDiv);
+        wrapper.innerHTML+= `<div class="box-red">` + i + `</div>`;
         console.log(i)
     }
+    // if (i % 3 == 0 && i % 5 == 0)
+    // {
+    //     let newDiv = document.createElement(`div`);
+    //     newDiv.classList.add(`box-blue`);
+    //     newDiv.innerHTML = "Fizzbuzz";
+    //     wrapper.append(newDiv);
+    //     console.log("FizzBuzz");
+
+    // } else if (i % 5 == 0) 
+    // {
+    //     let newDiv = document.createElement(`div`);
+    //     newDiv.classList.add(`box-red`);
+    //     newDiv.innerHTML = "Buzz";
+    //     wrapper.append(newDiv);
+    //     console.log("Buzz");
+    // } else if (i % 3 == 0)
+    // {
+    //     let newDiv = document.createElement(`div`);
+    //     newDiv.classList.add(`box-blue`);
+    //     newDiv.innerHTML = "Fizz";
+    //     wrapper.append(newDiv);
+    //     console.log("Fizz");
+
+    // } else 
+    // {
+    //     let newDiv = document.createElement(`div`);
+    //     newDiv.classList.add(`box-red`);
+    //     newDiv.innerHTML = i;
+    //     wrapper.append(newDiv);
+    //     console.log(i)
+    // }
 
 }
