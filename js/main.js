ed352f6d91cecb5f165d17ac5963d3987d361a18
giagra91 <giagra91@gmail.com>
2022-02-24 15:19:46 +0100

    modifiche classi su JS e box su CSS

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0)
     {
         let newDiv = document.createElement(`div`);
-        newDiv.classList.add(`box`);
+        newDiv.classList.add(`box-blue`);
         newDiv.innerHTML = "Fizzbuzz";
         wrapper.append(newDiv);
         console.log("FizzBuzz");
@@ -16,14 +16,14 @@ for (let i = 1; i <= 100; i++) {
     } else if (i % 5 == 0) 
     {
         let newDiv = document.createElement(`div`);
-        newDiv.classList.add(`box`);
+        newDiv.classList.add(`box-red`);
         newDiv.innerHTML = "Buzz";
         wrapper.append(newDiv);
         console.log("Buzz");
     } else if (i % 3 == 0)
     {
         let newDiv = document.createElement(`div`);
-        newDiv.classList.add(`box`);
+        newDiv.classList.add(`box-blue`);
         newDiv.innerHTML = "Fizz";
         wrapper.append(newDiv);
         console.log("Fizz");
@@ -31,7 +31,7 @@ for (let i = 1; i <= 100; i++) {
     } else 
     {
         let newDiv = document.createElement(`div`);
-        newDiv.classList.add(`box`);
+        newDiv.classList.add(`box-red`);
         newDiv.innerHTML = i;
         wrapper.append(newDiv);
         console.log(i)
